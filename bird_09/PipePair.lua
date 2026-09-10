85011a6bdc7037ee507266c5f23b77401dab0ed3
Gabriel Serafim <gabrielserafim780@gmail.com>
2026-09-09 22:22:36 -0300

    Flappy_Bird-09 Counting points with a score state

---
+++ bird_09/PipePair.lua
@@ -0,0 +1,47 @@
+---------------------------------------------------------------------------
+--
+--  PipePair.lua 
+--
+--  URL: https://github.com/GaBz12z/flappy_bird_CS50
+--
+---------------------------------------------------------------------------
+
+PipePair = Class{}
+
+GAP_HEIGHT = 90
+
+function PipePair:init(y)
+    -- Initialize pipes past the end of the screen
+    self.x = VIRTUAL_WIDTH + 32
+
+    self.y = y
+
+    -- Instantiate a pair of pipes 
+    self.pipes = {
+        ['upper'] = Pipe('top', self.y),
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + GAP_HEIGHT)
+    }
+
+    self.remove = false
+end
+
+function PipePair:update(dt)
+    --[[
+        Remove the pipe if its beyond the left edge + PIPE_WIDTH, else keep moving from right to left
+    ]]
+    if self.x > -PIPE_WIDTH then
+        self.x = self.x - PIPE_SPEED * dt
+        self.pipes['lower'].x = self.x
+        self.pipes['upper'].x = self.x
+    else
+        self.remove = true
+    end
+end
+
+function PipePair:render()
+    for k, pipe in pairs(self.pipes) do
+        pipe:render()
+    end
+end
+
+
---
+++ bird_09/main.lua
@@ -0,0 +1,148 @@
+---------------------------------------------------------------------------
+--
+--  main.lua - The score update 
+--
+--  URL: https://github.com/GaBz12z/flappy_bird_CS50
+--
+---------------------------------------------------------------------------
+
+-- transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * rotationSpeed);
+
+push = require 'push'
+
+Class = require 'class'
+
+require 'Bird'
+
+require 'Pipe'
+
+require 'PipePair'
+
+require 'StateMachine'
+require 'states.BaseState'
+require 'states.PlayState'
+require 'states.ScoreState'
+require 'states.TitleScreenState'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+-- Ground and background starting scroll location on the x-axis
+local backgroundScroll_2 = 0
+local backgroundScroll = 0
+local groundScroll = 0
+
+-- Speed at which the sprites will move
+local BACKGROUND_2_SCROLL_SPEED = 25
+local BACKGROUND_SCROLL_SPEED = 35
+local GROUND_SCROLL_SPEED = 50
+
+-- Dimension in the sprite where the code loops the background 
+local BACKGROUND_LOOPING_POINT = 288
+local BACKGROUND_2_LOOPING_POINT = 288
+
+function love.load()
+    -- Point filtering
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    background_2 = love.graphics.newImage('background-2.png')
+    background = love.graphics.newImage('background.png')
+    ground = love.graphics.newImage('ground.png')
+
+    love.window.setTitle('Flappy Bird in Lua')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+
+    love.graphics.setFont(flappyFont)
+
+    -- Create the virtual canvas
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    --[[
+        Global state machine that is initialize and return functions onthe saveState class
+    ]]
+    gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['play'] = function() return PlayState() end,
+        ['score'] = function() return ScoreState() end
+    }
+    gStateMachine:change('title')
+
+    -- Initialize input table, you can add any table that you wnat
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push.resize(w, h)
+end
+
+function love.keypressed(key)
+    -- Add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+--[[
+    Used to store any key that is fired at that frame, is a extensions inside love.keyboard space
+]]
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+
+function love.update(dt)
+    --[[
+        Parallax values that changes each frame and advances a little until it reaches a certain value and resets to 0 in the image
+    ]]
+    backgroundScroll_2 = (backgroundScroll_2 + BACKGROUND_2_SCROLL_SPEED * dt) % BACKGROUND_2_LOOPING_POINT
+
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+    -- The StateMachine deals with the update of all states in its class
+    gStateMachine:update(dt)
+
+    -- Reset input table
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+    push:apply('start')
+
+    --[[
+        We draw our images shifted to the left by their looping point, eventually they will revert to 0 once both values in % are equal, the division will be = 0 and the images will be reverted to their x = 0
+    ]]
+    -- Draw the background at the negative looping point
+    love.graphics.draw(background_2, -backgroundScroll_2, 0)
+
+    -- Draw the background at the negative looping point
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    --[[
+        We render the background outside the StateMachine because the background is always visible, any other process are dealt by the state machine
+    ]]
+    gStateMachine:render()
+ 
+    -- Draw the ground on top of the background, towards the bottom of the screen, at its negative looping point
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    push:apply('end')
+end
